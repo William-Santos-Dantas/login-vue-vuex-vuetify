c391c699acb7e93cdf12fb9eab3ff35c5b607293
unknown <NidrahDev@protonmail.com>
2021-02-13 07:13:30 -0300

    login vuejs/vuetify

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import { state, mutations } from './default'
+
+import auth from './modules/auth'
+
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    modules: {
+        auth,
+    },
+    state,
+    mutations,
+})
+
+export default store
